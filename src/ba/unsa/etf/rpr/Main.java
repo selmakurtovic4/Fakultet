c77c11bc76f0c62cd8d144376680c92f8c332b0a
selma <selmakurtovic@live.com>
2021-12-11 17:13:57 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("A");
+    }
+}
