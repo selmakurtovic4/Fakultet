8c7fcfe24b6719556ebc44af1a3884e12c7bfa4c
selma <selmakurtovic@live.com>
2021-12-14 00:17:09 +0100

    Dodana metoda  izracunajNormu u klasu UgovorzaProfesora

--- src/ba/unsa/etf/rpr/Fakultet.java
+++ src/ba/unsa/etf/rpr/Fakultet.java
@@ -3,21 +3,12 @@ package ba.unsa.etf.rpr;
 import java.util.ArrayList;
 
 public class Fakultet {
-    public ArrayList<Profesor> getProfesori() {
-        return profesori;
-    }
+    ArrayList<UgovorZaProfesora>profesori;
+    ArrayList<UgovorZaStudenta> studenti;
+    ArrayList<CiklusStudija> ciklusiStudija;
 
-    public ArrayList<UgovorZaStudenta> getUgovori() {
-        return ugovori;
-    }
 
-    public ArrayList<Predmet> getIzborniPredmeti() {
-        return izborniPredmeti;
-    }
 
-    ArrayList<Profesor> profesori;
-    ArrayList<UgovorZaStudenta> ugovori;
-    ArrayList<Predmet> izborniPredmeti;
 
     public Fakultet() {
     }
--- src/ba/unsa/etf/rpr/UgovorZaProfesora.java
+++ src/ba/unsa/etf/rpr/UgovorZaProfesora.java
@@ -1,13 +1,42 @@
 package ba.unsa.etf.rpr;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class UgovorZaProfesora {
     private Profesor profesor;
     private ArrayList<Predmet> predmeti;
     private Integer norma;
+    private Fakultet fakultet;
 
-    public UgovorZaProfesora(Profesor profesor) {
+    public UgovorZaProfesora(Profesor profesor, Fakultet fakultet) {
         this.profesor = profesor;
+        this.fakultet = fakultet;
+    }
+
+    public void dodajPredmet(Predmet predmet) {
+        predmeti.add(predmet);
+    }
+
+    public Integer izracunajNormu() {
+        int norma = 0;
+
+        for (var predmet : predmeti) {
+            //ako je predmet medju obaveznim
+            if (fakultet.ciklusiStudija.stream()
+                    .map(ciklusStudija -> ciklusStudija.getSemestri())
+                    .flatMap(ArrayList<Semestar>::stream)
+                    .anyMatch(semestar -> semestar.getObavezniPredmeti().contains(predmet)))
+                norma = norma + (predmet.getBrojCasovaMjesecno() * 12);
+                //ako je izborni
+            else if (fakultet.studenti.stream()
+                    .anyMatch(ugovor -> ugovor.izborniPredmeti.contains(predmet)))
+                norma = norma + (predmet.getBrojCasovaMjesecno() * 12);
+
+
+        }
+        return norma;
+
     }
 }
