8e4a76dac2d5f04b4d9b0e87533973d338d54b77
selma <selmakurtovic@live.com>
2021-12-13 21:13:50 +0100

    Implementirana klasa UpisniList

---
+++ src/ba/unsa/etf/rpr/UpisniList.java
@@ -0,0 +1,46 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+
+public class UpisniList {
+    Student student;
+    CiklusStudija ciklusStudija;
+    ArrayList<Predmet> izborniPredmeti;
+    Integer sifraSemestra;
+
+    public UpisniList(Student student) {
+        this.student = student;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public CiklusStudija getCiklusStudija() {
+        return ciklusStudija;
+    }
+
+    public void setCiklusStudija(CiklusStudija ciklusStudija) {
+        this.ciklusStudija = ciklusStudija;
+    }
+
+    public ArrayList<Predmet> getIzborniPredmeti() {
+        return izborniPredmeti;
+    }
+
+    public void setIzborniPredmeti(ArrayList<Predmet> izborniPredmeti) {
+        this.izborniPredmeti = izborniPredmeti;
+    }
+
+    public Integer getSifraSemestra() {
+        return sifraSemestra;
+    }
+
+    public void setSifraSemestra(Integer sifraSemestra) {
+        this.sifraSemestra = sifraSemestra;
+    }
+}
