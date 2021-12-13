de8ac58927ff1cf79e4e2081ac1797e15234b721
selma <selmakurtovic@live.com>
2021-12-13 18:47:38 +0100

    Implementirana klasa Student

---
+++ src/ba/unsa/etf/rpr/Student.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+public class Student {
+    private String ime;
+    private String prezime;
+    private String indeks;
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getIndeks() {
+        return indeks;
+    }
+
+    public void setIndeks(String indeks) {
+        this.indeks = indeks;
+    }
+
+    public Student(String ime, String prezime, String indeks) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.indeks = indeks;
+    }
+}
