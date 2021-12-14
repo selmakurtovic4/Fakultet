71a2419ba49b1ecdc3855fc584170d851ceeb6f3
selma <selmakurtovic@live.com>
2021-12-14 15:28:07 +0100

    Ispravljena metoda dajPrepisOcjena u UgovorZaStudenta

--- src/ba/unsa/etf/rpr/UgovorZaStudenta.java
+++ src/ba/unsa/etf/rpr/UgovorZaStudenta.java
@@ -1,12 +1,16 @@
 package ba.unsa.etf.rpr;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UgovorZaStudenta {
     private Student student;
     private  CiklusStudija ciklusStudija;
     private ArrayList<Predmet> izborniPredmeti=new ArrayList<>();
     private Integer sifraSemestra;
+    HashMap<Integer, Map<Predmet, Integer>> prepisOcjena;
    public Integer dajSumuBodova(){
        int suma=izborniPredmeti.stream().reduce(0, (a,b)->a+ b.getBodovi(), Integer::sum);
        return suma+ ciklusStudija.dajSemestarPoSifri(sifraSemestra).getObavezniPredmeti()
@@ -53,8 +57,14 @@ public class UgovorZaStudenta {
         return izborniPredmeti;
     }
 
+    public HashMap<Integer, Map<Predmet, Integer>> dajPrepisOcjena(){
+      return prepisOcjena;
+    }
+    public void dodajOcjenu(Predmet predmet, Integer ocjena) {
 
+       prepisOcjena.put(sifraSemestra, Collections.singletonMap(predmet,ocjena));
 
+    }
     public void setSifraSemestra(Integer sifraSemestra) {
         this.sifraSemestra = sifraSemestra;
     }
