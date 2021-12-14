ba393e9e6fe2f9568a829d88ce05a9b3364cbdbe
selma <selmakurtovic@live.com>
2021-12-14 23:06:27 +0100

    Implementirana metoda equals i hashCode u klasi Profesor

--- src/ba/unsa/etf/rpr/Profesor.java
+++ src/ba/unsa/etf/rpr/Profesor.java
@@ -1,12 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Locale;
+import java.util.Objects;
+
 public class Profesor {
     private String ime;
     private String prezime;
   //  private UgovorZaProfesora ugovorZaProfesora;
 
 
-    public Profesor(String ime, String prezime, Integer norma) {
+    public Profesor(String ime, String prezime) {
         this.ime = ime;
         this.prezime = prezime;
 
@@ -27,6 +30,21 @@ public class Profesor {
     public void setPrezime(String prezime) {
         this.prezime = prezime;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profesor profesor = (Profesor) o;
+        return this.ime.equalsIgnoreCase(profesor.ime) && prezime.equalsIgnoreCase(profesor.prezime)
+                && ime.trim().equals(profesor.ime.trim()) && prezime.trim().equals(profesor.prezime.trim());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ime, prezime);
+    }
+
 
 
     /*public UgovorZaProfesora getUgovorZaProfesora() {
