a2402212f71dd17c306b02d08e74a28b9306efca
selma <selmakurtovic@live.com>
2021-12-15 17:01:50 +0100

    Implementirana metoda equals u klasi Student

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
 public class Student {
     private String ime;
     private String prezime;
@@ -37,5 +39,18 @@ public class Student {
     @Override
     public String toString() {
         return  ime + " " + prezime + " "+ indeks+"\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return  Objects.equals(indeks, student.indeks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ime, prezime, indeks);
     }
 }
